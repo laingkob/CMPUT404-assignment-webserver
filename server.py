baf4459915946818020946a7b3f11dd8e053fc39
Courtenay Laing-Kobe <laingkob@cs.ualberta.ca>
2023-01-27 12:46:27 -0700

    Lab machine test, still need to fix mimetypes

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #  coding: utf-8 
 import socketserver
 from pathlib import Path
+import mimetypes
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos, Courtenay Laing-Kobe
 # 
@@ -31,13 +32,13 @@ str_200 = "200 OK"
 str_301 = "301 Moved Permanently"
 str_405 = "405 Method Not Allowed"
 str_404 = "404 Path Not Found"
-base = "http://127.0.0.1:8080/"
+base_url = "http://127.0.0.1:8080/"
 
 #TODO:
 # -MIME for css & html
 # -Fix 301 redirect
-# -Test on Lab Machine, check curl for 405 testing
-# -Make sure script runs ^
+# /Test on Lab Machine, check curl for 405 testing
+# /Make sure script runs ^
 # -More basic html error page
 # -Should add dates and content lengths to headers
 # -Check licensing
@@ -46,6 +47,9 @@ base = "http://127.0.0.1:8080/"
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        #default content type
+        content_type = "Content-Type: application/octet-stream"
+
         self.data = self.request.recv(1024).strip().decode('utf-8')
         print ("Got a request of: %s\n" % self.data)
         if self.data:
@@ -57,12 +61,17 @@ class MyWebServer(socketserver.BaseRequestHandler):
         if 'GET' != split_data[0]:
             self.code = str_405
         else:
-            path = 'www' + split_data[1]
+            if split_data[1][0:3] != "/www":
+                path = 'www' + split_data[1]
+            else:
+                path = split_data[1]
+
             if Path(path).is_dir():
                 if path[-1] != '/':
                     self.code = str_301
                     loc = path + '/'
                 path = path + 'index.html'
+
             if Path(path).is_file():
                 f = open(path, "r")
                 html = f.read()
@@ -70,27 +79,33 @@ class MyWebServer(socketserver.BaseRequestHandler):
             elif self.code != str_301:
                 self.code = str_404
 
+        mime_types = mimetypes.read_mime_types(path)
+        if mime_types != None:
+            split_path = path.split('.')
+            extension = '.' + split_path[-1]
+            content_type = "Content-Type: " + mime_types[extension] 
+
         if self.code == str_200:
             self.request.sendall(bytearray(f"HTTP/1.1 {self.code}\r\n\r\n" + html, 'utf-8'))
         
         elif self.code == str_301:
-            self.request.sendall(bytearray(f"HTTP/1.1 {self.code}\r\nLocation: {base + loc}\r\n\
-                Content-Type: text/html\r\n\
+            self.request.sendall(bytearray(f"HTTP/1.1 {self.code}\r\n\
+                Location: {base_url + loc}\r\n\
                 \r\n\
                 <html>\n\
                 <head><title>{self.code}</title></head>\n\
                 <body bgcolor=\"white\">\n\
                 <center><h1>{self.code}</h1></center>\n\
-                <hr><center>nginx/1.13.12</center>\n\
                 </body>\n\
-                </html>", 'utf-8'))         #CHANGE HTML TO MORE BASIC
+                </html>", 'utf-8'))         
         else:
-            self.request.sendall(bytearray(f"HTTP/1.1 {self.code}\r\n\r\n\
+            self.request.sendall(bytearray(f"HTTP/1.1 {self.code}\r\n\
+                {content_type}\r\n\
+                \r\n\
                 <html>\n\
                 <head><title>{self.code}</title></head>\n\
                 <body bgcolor=\"white\">\n\
                 <center><h1>{self.code}</h1></center>\n\
-                <hr><center>nginx/1.13.12</center>\n\
                 </body>\n\
                 </html>", 'utf-8'))
 
